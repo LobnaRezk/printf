11f4c5d28a3dc08e78325d416af1c40859601b02
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-25 14:48:19 -0700

    a

---
+++ printf_char.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * printf_char - prints a char.
+ * @ap: argument of function printf_char
+ * Return: 1.
+ */
+int printf_char(va_list ap)
+{
+	char c;
+
+	c = va_arg(ap, int);
+	_putchar(c);
+	return (1);
+}
