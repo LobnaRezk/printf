857cd25fe7b4025cfe89c43dce4f85feab7f9bea
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-28 07:54:07 -0700

    a

--- write_handlers.c
+++ write_handlers.c
@@ -117,41 +117,4 @@ write(1, &buffer[ind], length - (1 - padd_start)));
 if (extra_c)
 buffer[--ind] = extra_c;
 return (write(1, &buffer[ind], length));
-}
-/**
- * write_unsgnd - Writes an unsigned number
- * @is_negative: Number indicating if the num is negative
- * @ind: Index at which the number starts in the buffer
- * @buffer: Array of chars
- * @flags: Flags specifiers
- * @width: Width specifier
- * @precision: Precision specifier
- * @size: Size specifier
- * Return: Number of written chars.
- */
-int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width,
-int precision, int size)
-{
-int length = BUFF_SIZE - ind - 1, i = 0;
-char padd = ' ';
-UNUSED(is_negative);
-UNUSED(size);
-if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
-return (0);/* printf(".0d", 0)  no char is printed */
-if (precision > 0 && precision < length)
-padd = ' ';
-for (; precision > length; length++)
-buffer[--ind] = '0';
-if ((flags & F_ZERO) && !(flags & F_MINUS))
-padd = '0';
-if (width > length)
-{
-for (i = 0; i < width - length; i++)
-buffer[i] = padd;
-buffer[i] = '\0';
-(flags & F_MINUS) ? return (write(1, &buffer[ind], length
-+ write(1, &buffer[0], i)) : return (write(1, &buffer[0], i) +
-write(1, &buffer[ind], length));
-return (write(1, &buffer[ind], length));
-}
-}
+
