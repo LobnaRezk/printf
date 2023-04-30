a18ba9186d17e889cde0d3ef4bb208ede614d4cc
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-30 04:35:54 -0700

    a

--- utils.c
+++ utils.c
@@ -56,9 +56,9 @@ return (0);
 long int convert_size_number(long int num, int size)
 {
 if(size == S_LONG)
-return (num)
+return (num);
 else if (size == S_SHORT)
-return ((short)num)
+return ((short)num);
 return ((int)num);
 }
 /**
