d1517355f2153c73f02ba7d07ff64c9f898115a4
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-30 04:22:16 -0700

    a

--- utils.c
+++ utils.c
@@ -55,19 +55,18 @@ return (0);
 
 long int convert_size_number(long int num, int size)
 {
-(size == S_LONG) ? return (num) : (size == S_SHORT) ? return ((short)num) :
-return ((int)num);
+(size == S_LONG) ? (return (num)) : (size == S_SHORT) ? return (((short)num)) :
+(return ((int)num));
 }
 /**
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- *
  * Return: Casted value of num
  */
 
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-size == S_LONG ? return (num) : size == S_SHORT ? return ((unsigned short)num)
-: return ((unsigned int)num);
+size == S_LONG ? (return (num)) : size == S_SHORT ? (return ((unsigned short)num))
+: (return ((unsigned int)num));
 }
