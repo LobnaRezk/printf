ee8bdfe71d3cd76b21f7f0fd4762b2476403f30a
LobnaRezk <Lubna2018170788@cis.asu.edu.eg>
2023-04-30 02:58:19 -0700

    a

--- printf_binary.c
+++ printf_binary.c
@@ -23,14 +23,14 @@ UNUSED(width);
 UNUSED(precision);
 UNUSED(size);
 n = va_arg(types, unsigned int);
-m = 2147483648; 
+m = 2147483648;
 a[0] = n / m;
 i = 1;
 while (i < 32)
 {
 m /= 2;
 a[i] = (n / m) % 2;
- i++;
+i++;
 }
 i = 0, sum = 0, count = 0;
 while (i < 32)
@@ -42,7 +42,7 @@ char z = '0' + a[i];
 write(1, &z, 1);
 count++;
 }
- i++;
+i++;
 }
 return (count);
 }
